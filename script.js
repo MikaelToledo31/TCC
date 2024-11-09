884c39af9fc9beef677c6f865a9d52d893cebe81
Mikael Toledo <Mikaeltoledo31@gmail.com>
2024-11-09 09:25:35 -0300

    ajustes finais

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 document.getElementById('cadastroCliente').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário
+    event.preventDefault(); 
 
-    // Captura os valores dos campos
     const clientName = document.getElementById('name').value;
     const clientCpf = document.getElementById('cpf').value;
 
-    // Cria uma nova linha na tabela
     const newRow = document.createElement('tr');
     newRow.innerHTML = `<td>${clientName}</td><td>${clientCpf}</td>`;
 
-    // Adiciona a nova linha ao corpo da tabela
     document.querySelector('#clientTable tbody').appendChild(newRow);
 
-    // Limpa os campos do formulário
     document.getElementById('cadastroCliente').reset();
 });
